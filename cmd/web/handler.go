4efd86079c349215c6afbf52cae5ea9005a14e53
ymoldabe <MRanle91@gmail.com>
2023-07-27 02:53:39 +0600

    Chapter_7_full

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -15,6 +15,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
 	forums, err := app.forum.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -27,6 +33,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "home.tmpl", data)
 }
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -47,7 +59,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "view.tmpl", data)
 
 }
+
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", "POST")
 		app.clientError(w, http.StatusMethodNotAllowed)
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	mux.HandleFunc("/snippet/create/post", app.snippetCreatePost)
 
 	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
